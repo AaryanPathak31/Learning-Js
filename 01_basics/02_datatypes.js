97012995457e8e827b8318de30321fc51cfee1ba
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-17 18:47:16 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,4 +3,27 @@ in past many features were not there but with time new tags were added in js */
 
 "use strict"; //treat all Js code as newer version. i.e. new standards. this is also 
 //automatic but used as good syntax.
-alert(3+3)
+//alert(3+3) //we are using nodejs so we can't use it now.
+console.log(3+3) //code redability should be high so proper spaces should be there
+console.log('Aaryan')
+
+let name="Aaryan" //string datatype
+let age=19 // number data type 
+let isLoggedIn= false //boolean datatype
+
+
+/* Primitive  data type
+number => 2 ro power 53
+bigint=> high number than number
+string=> ""
+noolean=> true/false
+Null => standalone value too . --> empty datatype. nothing is there.. void.
+Undefined => not defined till now i.e. just make it's variable
+symbol => for uniqueness.
+
+#objects
+*/
+console.log(typeof"Aaryan")
+console.log(typeof age);
+console.log(typeof null); //type is object
+console.log(typeof undefined); //type is object
