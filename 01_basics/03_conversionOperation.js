2771c6db742f6b6bd18126f2c11e242965eec348
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-18 09:40:28 +0000

    comparrision_start_01

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -60,9 +60,68 @@ let str1="hello"
 let str2=" Aaryan "
 
 let str3=str1+str2
-console.log(str1+str2)
-console.log(str3)
+// console.log(str1+str2)
+// console.log(str3)
 
-//problem
-console.log("1"+2);
-console.log(1+"2");
+// //problem
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1" + 2 +"2"); //122 /* not dependable on these */
+// console.log(1+2+"2"); //33
+
+// console.log(3+4*5%3); //use paranthesis
+// console.log((3+4)*5%3);
+
+// console.log(true);
+// console.log(+true); //value is 1
+// // console.log(true+); //error code
+// console.log(+""); //0 is output
+
+//Operatorn presedence to be learned.
+
+let num1, num2, num3
+num1=num2=num3=2+2
+// console.log([num1,num2,num3]);
+// above should be avoided
+
+let gameCounter =100
+// gameCounter++ //add 1 to 100 i.e. 101
+
+++gameCounter
+console.log(gameCounter)
+
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+/*
+Postfix increment
+JS
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+Prefix increment
+JS
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+*/
+
+
