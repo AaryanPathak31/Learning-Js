d4ae2669aac270e42f55b4df1b2e5c90dc32b1f2
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-18 08:50:12 +0000

    string operations 1

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,8 +37,32 @@ empty string "" => false
 "Aaryan" => true
 */
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber =String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber); //typeof is string.
+// let stringNumber =String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber); //typeof is string.
+
+
+/***********************Operations************ */
+
+let value=3
+let negvalue= -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //2^2
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Aaryan "
+
+let str3=str1+str2
+console.log(str1+str2)
+console.log(str3)
+
+//problem
+console.log("1"+2);
+console.log(1+"2");
