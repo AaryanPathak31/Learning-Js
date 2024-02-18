ba6e5c691dbc0049334a8cd0fe08d260f08d14c2
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-18 08:42:01 +0000

    Conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,44 @@
-let score= 33
+//let score= "33abc"
+//let score= null //typeof => 0
+//let score= undefined //typeof => Nan
+//let score= true //typeof=> 1 asit is true.
+// let score= "Aaryan" //typeof=> Nan
 
-console.log(type)
+// console.log(typeof score);
+// console.log(typeof(score)); //both are same
+
+
+// let valueInNumber=Number(score)
+//it would turn score in number even it is in string.
+// it would even turn string value to numbers and show its types as NaN
+// not be dependable on numbers in Js
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//NaN => Not a Number!
+/*
+"33" => 33
+"33abc" => Nan
+true => 1; false=>0
+*/
+
+// let isLoggedIn=true
+//let  isLoggedIn=1
+//let  isLoggedIn="" //=> false
+//let  isLoggedIn="Aaryan"
+
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true ;
+0=> false
+empty string "" => false
+"Aaryan" => true
+*/
+
+let someNumber = 33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber); //typeof is string.
