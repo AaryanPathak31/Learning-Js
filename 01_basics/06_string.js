edd8b620d60232b2ec3cf3afa53158a53aede131
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-21 16:44:08 +0000

    string2

---
+++ 01_basics/06_string.js
@@ -0,0 +1,8 @@
+const name="Aaryan"
+const repoCount=2
+
+// console.log(name + repoCount+ "Value");  => old value
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//above is the modern way to declare the string.
+
+// const gameName
