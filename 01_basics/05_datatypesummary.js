03f75b2a84b0bbba537c0ab126c128adb0215476
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-21 16:36:28 +0000

    heap and Stack

--- 01_basics/05_datatypesummary.js
+++ 01_basics/05_datatypesummary.js
@@ -23,34 +23,85 @@ No float here.
                 -Objects
                 -Functions
  */
-                const score = 100
-                const scoreValue = 100.3
-                
+                // const score = 100
+                // const scoreValue = 100.3
+                /*
                 const isLoggedIn = false
                 const outsideTemp = null
-                let userEmail;
+                let userEmail; //undefined
                 
                 const id = Symbol('123')
-                const anotherId = Symbol('123')
+                const anotherId = Symbol('123')*/
                 
-                console.log(id === anotherId);
+                // console.log(id === anotherId); //false
                 
-                // const bigNumber = 3456543576654356754n
+                // const bigNumber = 3456543576654356754n //n make it automatically big int
                 
                 
                 
                 // Reference (Non primitive)
                 
                 // Array, Objects, Functions
-                
+               /* 
                 const heros = ["shaktiman", "naagraj", "doga"];
                 let myObj = {
-                    name: "hitesh",
+                    name: "Aaryan",
                     age: 22,
                 }
                 
                 const myFunction = function(){
                     console.log("Hello world");
-                }
+                }*/
                 
-                console.log(typeof anotherId);
+                // console.log(typeof anotherId); //symbol
+                // console.log(typeof myFunction); //function
+
+//return type of non-primitive type is object. also known as object_function.
+
+/**
+Type of val	Result
+Undefined	"undefined"
+Null	"object"
+Boolean	"boolean"
+Number	"number"
+String	"string"
+Object (native and does not implement [[Call]])	"object"
+Object (native or host and does implement [[Call]])	"function"
+Object (host and does not implement [[Call]])	
+        Implementation-defined except may not be "undefined", 
+        "boolean", "number", or "string".
+ */
+
+//*********** */        //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//************************************** */
+
+//Stack(Primitive) And Heap(Non-Primitive)
+//stack => ek ke uppar ek.
+//heap large storage box.
+/*whenevr we use stack we get memory
+and in heap we get refrence of original value*/
+
+let myname="Aaryan"
+
+let anothername=myname
+anothername="Pathak"
+
+console.log(myname) //stiall have Aaryan
+console.log(anothername)
+//above is stack so we have copy
+let userone={
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+console.log(userone); 
+//{ email: 'user@gmail.com', upi: 'user@ybl'} <=joutput
+//above is stored in heap so change at the original. in the above
+
+let userTwo=userone
+
+userTwo.email="aaryan@google.com"
+console.log(userone.email)
+console.log(userTwo.email)
+//both have aaryan@google.com
+
