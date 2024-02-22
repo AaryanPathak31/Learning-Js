0612eb4f8b1e3b938ab83bf8a6342fd2080d3fd1
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-22 13:43:26 +0000

    stingsdone

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -2,7 +2,43 @@ const name="Aaryan"
 const repoCount=2
 
 // console.log(name + repoCount+ "Value");  => old value
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 //above is the modern way to declare the string.
 
-// const gameName
+const gameName=new String('Aaryanap')
+/*
+String {'Aaryanap'}
+0: "A"
+1: "a"
+2: "r"
+3: "y"
+4: "a"
+5: "n"
+6: "a"
+7: "p"
+length: 8
+[[Prototype]]: String
+[[PrimitiveValue]]: "Aaryanap"
+*/
+// console.log(gameName[0]); //=>A
+// console.log(gameName.__proto__); //=> {} //this is syntax
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase()); //print all element in the string.
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('r'));
+
+const newString=gameName.substring(0,4)
+console.log(newString); //output=> Aary till 3rd index only.
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne="     Aaryan       "
+console.log(newStringOne);
+console.log(newStringOne.trim());  //trim only works on blank spaces.
+
+const url="https://www.mtlnovel.com/reborn-capital-empire/chapter-12-seize-the-future/"
+console.log(url.replace('reborn', '-')) //replace reborn with -
