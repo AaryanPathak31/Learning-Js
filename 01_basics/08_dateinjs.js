e123ef40979d87113f70c36ef0ebeeac2a086b0e
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-26 13:45:05 +0000

    date1

--- 01_basics/08_dateinjs.js
+++ 01_basics/08_dateinjs.js
@@ -1,1 +1,28 @@
-console.log("hello");
+// console.log("hello");
+//Dates
+
+let myDate=new Date()
+// console.log(myDate);** from down here
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString())
+console.log(myDate.getTimezoneOffset())
+
+/*
+Output in Sequence:-
+Mon Feb 26 2024 13:43:07 GMT+0000 (Coordinated Universal Time)
+2/26/2024
+Mon Feb 26 2024
+2024-02-26T13:43:07.980Z
+2024-02-26T13:43:07.980Z
+2/26/2024, 1:43:07 PM
+1:43:07 PM
+13:43:07 GMT+0000 (Coordinated Universal Time)
+*/
+
+
