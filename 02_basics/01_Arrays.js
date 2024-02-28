fa4276911d919ff1b989600fcefde679bd21ec65
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-02-28 17:33:37 +0000

    organising

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,27 @@
+//Arrays
+const myArr=[0,1,2,3,4,5,true,'Aaryan']
+//each element is called element.
+
+/*
+The Array object, as with arrays in other programming languages, 
+enables storing a collection of multiple items under a single
+ variable name, and has members for performing common array operations.
+
+javaScript arrays are resizable and can contain a mix of different data types.
+
+any type of data type could be stored.
+
+*/
+// console.log(myArr[0]); //index start from zero
+console.log(myArr[6]);  
+/*array copy operation make shallow copies
+create shallow copies.
+**mdn shallow copy => chnage in original too
+**Deep copies=> property do not share the same refrence.
+*/
+
+//**********Methods ************************/
+cont myHeroes=["Shaktiman","Iron Man","Thor","Loki"]
+
+const myArr2=new Array(1,2,3,4)
+console.log(myArr[0]);
