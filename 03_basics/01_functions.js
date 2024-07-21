33f41708db872ca7afbde01127d38fb238003d7c
AaryanPathak31 <160325477+AaryanPathak31@users.noreply.github.com>
2024-07-21 19:25:25 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -66,5 +66,7 @@ function returnSecondValue(getArray){
     return getArray[1]
 }
 
+
+//**********global object in browser is window.**********************
 // console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
